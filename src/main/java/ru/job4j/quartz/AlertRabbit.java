15efcd4bfc48d3413ade402cf8ab86d16b4a0827
alex <alexterentev90@gmail.com>
2021-12-04 12:18:31 +0300

    1. Quartz [#260360 #214221] fix

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,9 +3,9 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -15,60 +15,40 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
-    private static Properties properties;
 
-    private static void readProp() {
-        properties = new Properties();
-        try (InputStream in = AlertRabbit.class.getClassLoader()
-                .getResourceAsStream("rabbit.properties")) {
+    private static Properties readProperties() throws IOException {
+        Properties properties = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
             properties.load(in);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+        return properties;
     }
 
-    private static int getInterval() {
-        readProp();
-        int rsl = 0;
-        try {
-            rsl = Integer.parseInt(properties.getProperty("rabbit.interval"));
-        } catch (Exception e) {
-            throw new IllegalArgumentException("file rabbit.properties is corrupted");
-        }
-        return rsl;
-    }
-
-    private static Connection initConnection() {
-        readProp();
+    private static Connection initConnection(Properties properties) {
         Connection cn = null;
         try {
             Class.forName(properties.getProperty("driver-class-name"));
             cn = DriverManager.getConnection(
                     properties.getProperty("url"),
                     properties.getProperty("username"),
-                    properties.getProperty("password")
-            );
+                    properties.getProperty("password"));
         } catch (Exception e) {
             e.printStackTrace();
         }
         return cn;
     }
 
-    public static void main(String[] args) {
-        Connection cn = initConnection();
-        String sql = "insert into rabbit(created_date) values(?);";
-        try {
+    public static void main(String[] args) throws Exception {
+        Properties pr = readProperties();
+        try (Connection cn = initConnection(pr)) {
             List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDataMap data = new JobDataMap();
             data.put("connection", cn);
-            data.put("sql", sql);
-            JobDetail job = newJob(Rabbit.class)
-                    .usingJobData(data)
-                    .build();
+            JobDetail job = newJob(Rabbit.class).usingJobData(data).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(5)
+                    .withIntervalInSeconds(Integer.parseInt(pr.getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -77,37 +57,27 @@ public class AlertRabbit {
             scheduler.scheduleJob(job, trigger);
             Thread.sleep(10000);
             scheduler.shutdown();
-            System.out.println(store);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
-        private LocalDateTime created = LocalDateTime.now();
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
 
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
-            Connection cn = (Connection) context.getJobDetail().getJobDataMap().get("connection");
-            String sql = (String) context.getJobDetail().getJobDataMap().get("sql");
-            this.add(cn, sql);
-        }
-
-        private void add(Connection cn, String sql) {
-            try (PreparedStatement statement = cn.prepareStatement(sql)) {
-                statement.setTimestamp(1, getTimestamp(this.created));
+            String sql = "insert into rabbit(created_date) values (?)";
+            System.out.println("Rabbit runs here ...");
+            Connection connection = (Connection) context.getJobDetail()
+                    .getJobDataMap().get("connection");
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
                 statement.execute();
-            } catch (Exception e) {
+            } catch (SQLException e) {
+                System.out.println("Ошибка в исполняемом методе: execute");
                 e.printStackTrace();
             }
-        }
-
-        private LocalDateTime getLocalDateTime(Timestamp timestamp) {
-            return timestamp.toLocalDateTime();
-        }
-
-        private Timestamp getTimestamp(LocalDateTime localDateTime) {
-            return Timestamp.valueOf(localDateTime);
         }
     }
 }
